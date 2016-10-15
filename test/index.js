e280f714afc18d468dcd67601bd4359ce33885de
Mário Carneiro <mariocarneiro@musikki.com>
2016-10-15 09:23:20 +0100

    more unit tests

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
 
 const chai = require('chai');
 const mocha = require('mocha');
-const sinon = require('sinon');
+const fetchMock = require('fetch-mock');
 
 const expect = chai.expect;
 
@@ -11,7 +11,7 @@ const Musikki = require('../lib');
 
 let musikki;
 
-describe('constructor', function() {
+describe('api constructor', function() {
 
     it('should return an instance of the Musikki class', function() {
 
@@ -24,22 +24,20 @@ describe('constructor', function() {
 
     it('should fail when not given an appid or appkey', function() {
 
-        expect(function() {
-            return new Musikki();
-        }).to.throw(Error, 'AssertionError: no appid given');
+        expect(() => new Musikki())
+            .to.throw(Error, 'AssertionError: no appid given');
 
-        expect(function() {
-            return new Musikki({ appid: 'fakeappid' });
-        }).to.throw(Error, 'AssertionError: no appkey given');
+        expect(() => new Musikki({ appid: 'fakeappid' }))
+            .to.throw(Error, 'AssertionError: no appkey given');
 
 
     });
 
 });
 
-describe('method loading from specification json', function() {
+describe('interface with methods loaded from specification json', function() {
 
-    it('instance should contain endpoint functions', function() {
+    it('should contain endpoint functions', function() {
 
          Object.keys(spec.endpoints).forEach((endpoint_name) => {
             expect(musikki).to.have.property(endpoint_name);
@@ -53,7 +51,6 @@ describe('method loading from specification json', function() {
         Object.keys(spec.endpoints).forEach((endpoint_name) => {
 
             let methods = musikki[endpoint_name]();
-            console.log(methods);
             Object.keys(spec.endpoints[endpoint_name]).forEach((method_name) => {
 
                 expect(methods).to.have.property(method_name);
@@ -62,6 +59,71 @@ describe('method loading from specification json', function() {
             });
 
         });
+
+    });
+
+});
+
+describe('musikki api usage', function() {
+
+    before(() => {
+        fetchMock.get('*', { result: 'ok' });
+    });
+
+    after(() => {
+        fetchMock.restore();
+    });
+
+    it('using endpoints that require an mkid should reject with error when not given one', function() {
+
+        /* results is an array of array of promises */
+        let promiseArrayArray = Object.keys(spec.endpoints).map((endpoint_name) => {
+
+            let methods = musikki[endpoint_name]();
+
+            return Object.keys(spec.endpoints[endpoint_name]).map((method_name) => {
+
+                let method = methods[method_name];
+                let method_spec = spec.endpoints[endpoint_name][method_name];
+
+                if (method_spec.indexOf('{mkid}') !== -1) {
+                    return method()
+                        .catch((error) => {
+                            expect(error).to.be.instanceof(Error)
+                                .and.have.property('message', 'this endpoint requires an mkid');
+                        });
+                }
+
+            }).filter((value) => !!value);
+
+        }).filter((value) => value.length > 0);
+
+        return Promise.all(promiseArrayArray.map((promiseArray) => Promise.all(promiseArray)));
+
+    });
+
+    it('invoking api method functions should trigger http calls to the api', function() {
+
+        /* results is an array of array of promises */
+        let promiseArrayArray = Object.keys(spec.endpoints).map((endpoint_name) => {
+
+            let methods = musikki[endpoint_name]('fakemkid');
+
+            return Object.keys(spec.endpoints[endpoint_name]).map((method_name) => {
+
+                let method = methods[method_name];
+                let method_spec = spec.endpoints[endpoint_name][method_name];
+
+                return method()
+                    .then((response) => {
+                        expect(response.result).to.equal('ok');
+                    });
+
+            }).filter((value) => !!value);
+
+        }).filter((value) => value.length > 0);
+
+        return Promise.all(promiseArrayArray.map((promiseArray) => Promise.all(promiseArray)));
 
     });
 
